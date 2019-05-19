d19d16d36169dbb616a72b670c507c5434c4b3e0
AUBOIRON <auboiron_sacha@yahoo.fr>
2019-05-19 11:50:23 +0200

    maj produit

--- vues/v_produits.php
+++ vues/v_produits.php
@@ -1,22 +1,28 @@
-<div id="produits">
-<?php
-echo "<table>";
-foreach( $lesProduits as $unProduit) 
-{
-	$id = $unProduit->getId();
-	$description = $unProduit->getDescription();
-	$image = $unProduit->getImage();
-	$prix = $unProduit->getPrix();
-        echo "<form method='POST' action='index.php?uc=voirProduits&idCategorie=$idCategorie&idProduit=$id&action=voirProduitsEtAjouterAuPanier'>";
-	echo "<tr>
-			<td width=150><img src='$image' alt='image' width='100'	height='100' /></td>
-			<td width=100>$description</td>
-			<td width=100>$prix</td>
-			<td width=300> Quantite :<input type='text' name='quantite' value ='1' size='2' />
-                        <input type='submit' value='Commander'  /></td>";
-	echo '</tr>';
-        echo "</form>";
-}
-?>
-</table>
+<div class="container">
+	<div class="row">
+		<div class="col" style="margin-left:150px;margin-top:50px">
+			<div id="produits">
+			<?php
+			echo "<table>";
+			foreach( $lesProduits as $unProduit) 
+			{
+				$id = $unProduit->getId();
+				$description = $unProduit->getDescription();
+				$image = $unProduit->getImage();
+				$prix = $unProduit->getPrix();
+				echo "<form method='POST' action='index.php?uc=voirProduits&idCategorie=$idCategorie&idProduit=$id&action=voirProduitsEtAjouterAuPanier'>";
+				echo "<tr>
+						<td width=150><img src='$image' alt='image' width='100'	height='100' /></td>
+						<td width=100>$description</td>
+						<td width=100>$prix €</td>
+						<td width=600> Quantite :<input type='text' name='quantite' value ='1' size='2' />
+						<input type='submit' value='Commander'  /></td>";
+				echo '</tr>';
+				echo "</form>";
+			}
+			?>
+			</table>
+			</div>
+		</div>
+	</div>
 </div>
